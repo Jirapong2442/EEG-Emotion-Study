12b022c4837e21d19bdfd5f1b4317b43dc3c6a74
Jirapong <icejirapong@gmail.com>
2025-07-18 15:09:39 +0800

    add volume reduction

--- resize_vdo.py
+++ resize_vdo.py
@@ -2,8 +2,8 @@ import os
 from moviepy.editor import *
 
 # Define input and output parameters
-path_folder = "C:/Users/NA/Saved Games/cloned/test_reels2/original/"
-output_path = "C:/Users/NA/Saved Games/cloned/test_reels2/"
+path_folder = "C:/Users/NA/Saved Games/cloned/reels3/original/"
+output_folder = "C:/Users/NA/Saved Games/cloned/reels3/"
 new_width = 450
 new_height = 800
 
@@ -13,23 +13,24 @@ for file in os.listdir(path_folder):
         file_name = file[:-4]
         out_name = "resized_" + file
         input_path = os.path.join(path_folder, file)
-        output_path = os.path.join(path_folder, out_name)
+        out_path = os.path.join(output_folder, out_name)
 
         try:
             # Load the video file
             video = VideoFileClip(input_path)
+            video = video.volumex(0.8)
 
             # Resize (crop) the video to the desired dimensions
             resized_video = video.resize((new_width, new_height))
 
             # Write the output video with audio
-            resized_video.write_videofile(output_path, codec='libx264', audio_codec='aac')
+            resized_video.write_videofile(out_path, codec='libx264', audio_codec='aac')
 
             # Close the video file to free resources
             resized_video.close()
             video.close()
 
-            print(f"Video cropped and saved with audio as {output_path}")
+            print(f"Video cropped and saved with audio as {out_path}")
 
         except Exception as e:
             print(f"Error processing {file}: {str(e)}")
